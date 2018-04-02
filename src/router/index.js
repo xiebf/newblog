e121592b9aea3cd66cab6d93033bef33feca174f
xiebinfeng <xiebinfeng@wdai.com>
2018-04-02 16:54:31 +0800

    update code

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,16 @@ import Hello from 'components/Hello'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        },
+        {
+            path: 'detail',
+            name: 'detal',
+            component: Hello
+        }
+    ]
 })
